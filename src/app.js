3b82d2ba6006f260dad318844acc4866039fea11
Zayed Ahmed <zayed967@yahoo.com>
2026-06-09 12:49:49 -0400

    form ready

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
-let tickets = []
+// let tickets = []
 
-function addTicket(title, customer) {
-    const ticket = {
-        id: Date.now(),
-        title,
-        customer,
-        status: "New",
-        priority: "Medium",
-        createdAt: new Date().toISOString()
-    };
+// function addTicket(title, customer) {
+//     const ticket = {
+//         id: Date.now(),
+//         title,
+//         customer,
+//         status: "New",
+//         priority: "Medium",
+//         createdAt: new Date().toISOString()
+//     };
 
-    tickets.push(ticket);
-    renderTickets();
+//     tickets.push(ticket);
+//     renderTickets();
 
-}
+// }
 
+// Server setup
 const express = require("express");
 const path = require("path");
 
@@ -27,3 +28,24 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("http://localhost:3000");
 })
+
+// Database setup
+const Database = require("better-sqlite3");
+
+const db = new Database("tickets.db");
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS tickets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL
+    )
+`);
+
+// db.prepare(
+//     "INSERT INTO tickets (title, description) VALUES (?, ?)"
+// ).run("Computer issue","Computer will not start");
+
+// const tickets = db.prepare("SELECT * FROM tickets").all();
+
+// console.log(tickets);
