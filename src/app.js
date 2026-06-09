151a60b8390b8ad4ff8e7fe9a2dbdbffef3c5926
Zayed Ahmed <zayed967@yahoo.com>
2026-06-09 11:42:16 -0400

    git ignore

--- src/app.js
+++ src/app.js
@@ -14,3 +14,16 @@ function addTicket(title, customer) {
     renderTickets();
 
 }
+
+const express = require("express");
+const path = require("path");
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+})
